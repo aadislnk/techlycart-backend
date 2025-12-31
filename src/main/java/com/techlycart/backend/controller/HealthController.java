206723f5a629f8eb38fdcf6b4c9ca782091c7ae8
aadislnk <adityasohanslnk@gmail.com>
2025-12-31 21:10:12 +0530

    Add root endpoint and JSON health check

--- src/main/java/com/techlycart/backend/controller/HealthController.java
+++ src/main/java/com/techlycart/backend/controller/HealthController.java
@@ -2,12 +2,26 @@ package com.techlycart.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Map;
 @RestController
 public class HealthController {
 
+//    @GetMapping("/health")
+//    public String health() {
+//        return "TechlyCart Backend is running";
+//    }
+@GetMapping("/")
+public Map<String, String> root() {
+    return Map.of(
+            "message", "Welcome to TechlyCart Backend"
+    );
+}
+
     @GetMapping("/health")
-    public String health() {
-        return "TechlyCart Backend is running";
+    public Map<String, String> health() { //this method now returns an object
+        return Map.of(
+                "status", "UP",
+                "service", "TechlyCart Backend"
+        );
     }
 }
