36e04f836d212871f9c10bb68b3b57b046cc9209
aadislnk <adityasohanslnk@gmail.com>
2025-12-31 19:29:27 +0530

    Initialize Spring Boot project with health endpoint

---
+++ src/main/java/com/techlycart/backend/TechlycartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.techlycart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlycartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechlycartBackendApplication.class, args);
+
+	}
+}
