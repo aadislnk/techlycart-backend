149d169166c35631cfa88e201b4063aca94e0a36
aadislnk <adityasohanslnk@gmail.com>
2026-01-01 11:41:20 +0530

    learned about service layer

--- src/main/java/com/techlycart/backend/controller/ProductController.java
+++ src/main/java/com/techlycart/backend/controller/ProductController.java
@@ -7,38 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techlycart.backend.entity.Product;
-import com.techlycart.backend.repository.ProductRepository;
+import com.techlycart.backend.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
-    //above is constructor injection
-    /*
-    sees ProductRepository
-creates its implementation
-injects it automatically
-You didn’t write:
-new ProductRepository()
-This is Dependency Injection (we’ll formalize it later).*/
 
     @GetMapping
     public List<Product> getAllProducts() {
-        return productRepository.findAll();
+        return productService.getAllProducts();
     }
-    /*
-    findAll()
-productRepository.findAll();
-This:
-runs SELECT * FROM product
-maps rows → Product objects
-returns a List<Product>
-All without SQL.
-     */
 }
+
 
